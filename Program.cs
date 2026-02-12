8caa517fe6b2ac9d2f339b25cfe8012d90e5085e
Miguel57835 <mcalderonbarba@gmail.com>
2026-02-12 10:39:34 +0100

    leearchivo
    Para tú

--- Program.cs
+++ Program.cs
@@ -20,7 +20,42 @@
         }
         static Estado LeeNivel(string file, int n)
         {
-
+            Estado nivel;
+            StreamReader file = new StreamReader(file);
+            string l = "level " + n;
+            while (file.ReadLine() != l) ;
+            nivel.obj = file.Read();
+            file.ReadLine();
+            bool espacio = false;
+            string tablero, linea;
+            bool numColumna = false;
+            int numCol = 0;
+            int i = 0;
+            while (!espacio)
+            {
+                linea = file.ReadLine() + ' ';
+                if (!numColumna) { numColumna = true;  numCol = linea.GetLength; }
+                if (linea == " ") espacio = true;
+                else { tablero += linea; i++; }
+            }
+            nivel.mat = new char[numCol+2, i+2];
+            string[] lineas = tablero.Split (' ');
+            for (int i = 0; i < nivel.mat.GetLength[0];i++) nivel.mat[i, 0] = '#';
+            for (int i = 0; i < nivel.mat.GetLength[1]-2; i++)
+            {
+                for (int j = 0; i < nivel.mat.GetLength[0]; j++)
+                {
+                    nivel.mat[0] = '#';
+                    for (int k = 0;i< nivel.mat.GetLength[0]-2;k++)
+                    {
+                        nivel.mat[k + 1, i] = lineas[k][i];
+                    }
+                    nivel.mat[nivel.mat.GetLenght[0]] = '#';
+                }
+            }
+            for (int i = 0; i < mat.GetLength[0];i++) nivel.mat[i, mat.GetLenght[1]] = '#';
+            nivel.act.x = 1; nivel.act.y = 1;
+            return nivel;
         }
         static void Render(Estado est)
         {
@@ -50,7 +85,7 @@
         }
         static int BloqueToInt(char c)
         {
-            return ((int)c) - ((int) ’a’) +1;
+            return ((int)c) - ((int) 'a') +1;
         }
 
     }
