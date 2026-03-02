87f55bc27b05753e7787670d71f18655c21f20d4
Riuttasu <239182073+Riuttasu@users.noreply.github.com>
2026-03-02 16:34:37 +0100

    Pulido del documento
    Regiones, comentarios, nombres de variables...

--- Program.cs
+++ Program.cs
@@ -2,87 +2,109 @@
 {
     internal class Program
     {
-        // coordenadas (x,y) para representar posiciones y direcciones de desplazamiento
+        // Máximo de movimientos registrados en memoria para poder deshacerlos
         const int maxMem = 10;
+        // Sistema de coordenadas vertical-horizontal en la matriz bidimensional
         struct Coor
         {
-            public int x, y;
+            public int ver, hor;
         }
+        // Cuando se hace un movimiento de bloque, donde ha empezado y terminado el cursor
         struct Jugada
         {
             public Coor aca; //Posición final de la jugada
             public Coor emp; //Posición inicial de la jugada
         }
+        // Memoria de jugadas hechas por el jugador para poder deshacerlas si se requiere
         struct Memoria
         {
             public Jugada[] Jugadas; //Serie de jugadas
             public int ind; //Indice de la jugada actual
         }
+        // Estado del juego
         struct Estado
-        { // estado del juego
+        { 
             public char[,] mat; // '#' muro; '.' libre; letras 'a','b' ... bloques
             public char obj; // char correspondiente al bloque objetivo (el que hay que sacar)
             public Coor act, sal; // posiciones del cursor y de la salida
             public bool sel; // idica si hay bloque seleccionado para mover o no
         }
-        static void Main(string[] args)
+        static void Main()
         {
-            Memoria memoria; memoria.Jugadas = new Jugada[maxMem];
+            Memoria memoria; memoria.Jugadas = new Jugada[maxMem]; // Inicialización de la memoria de jugadas
             int level;
-            Estado Nivel;
+            Estado est; // Estado de juego
             bool hayJuego, victoria;
             while (true)
             {
-                memoria.ind = 0; 
+                memoria.ind = 0;
+                // Lectura de nivel
+                Console.Write("¿Qué nivel desea jugar?: ");
                 level = int.Parse(Console.ReadLine());
-
-                Console.CursorVisible = false;
-                Nivel = LeeNivel("levels.txt", level);
-                MarcaSalida(ref Nivel);
+                Console.CursorVisible = false; Console.Clear();
+                // Se inicializa el estado al nivel deseado
+                est = Leeest("levels.txt", level);
+                // Se marca la casilla de salida del nivel (condición de victoria)
+                MarcaSalida(ref est);
                 hayJuego = true;
                 victoria = false;
-
-                Render(Nivel);
+                // Render inicial
+                Render(est);
+                // Bucle principal de juego
                 while (hayJuego && !victoria)
                 {
+                    // Lectura de input
                     char c = LeeInput();
-
+                    // Procesamiento de input
                     if (c == 'q') hayJuego = false;
-                    else if (c == 'z') Delete(ref memoria, ref Nivel);
-                    else ProcesaInput(ref Nivel, c, ref memoria);
-                    Render(Nivel);
-                    if (Nivel.mat[Nivel.sal.y, Nivel.sal.x] == Nivel.obj) victoria = true;
+                    else if (c == 'z') Delete(ref memoria, ref est);
+                    else ProcesaInput(ref est, c, ref memoria);
+                    // Render del estado de juego
+                    Render(est);
+                    // Comprobación de condición de victoria
+                    if (est.mat[est.sal.ver, est.sal.hor] == est.obj) victoria = true;
                 }
+                // Si se ha logrado conseguir la condición de victoria
                 if (victoria)
                 {
                     Console.WriteLine("GANASTE");
                 }
+                // Si se ha salido del juego mediante input
                 else Console.Clear();
             }
         }
+        // Deshacer jugada
         static void Delete(ref Memoria mem, ref Estado est)
         {
+            // Solo deshace si hay jugada registrada
             if (mem.ind > 0)
             {
                 Coor dir;
-                mem.ind--;  
-                dir.x = mem.Jugadas[mem.ind % maxMem].emp.x - mem.Jugadas[mem.ind % maxMem].aca.x;
-                dir.y = mem.Jugadas[mem.ind % maxMem].emp.y - mem.Jugadas[mem.ind % maxMem].aca.y;
-                if (!(dir.x == 0 && dir.y == 0))
+                mem.ind--;  // Retrocede en el índice de jugadas registradas
+                // dir = coordenada de empezar - coordenada de acabar
+                dir.hor = mem.Jugadas[mem.ind % maxMem].emp.hor - mem.Jugadas[mem.ind % maxMem].aca.hor;
+                dir.ver = mem.Jugadas[mem.ind % maxMem].emp.ver - mem.Jugadas[mem.ind % maxMem].aca.ver;
+                // Solo mueve si no ha comenzado y acabado la última jugada en la misma coordenada
+                if (!(dir.hor == 0 && dir.ver == 0))
                 {
-                    est.act.x = mem.Jugadas[mem.ind % maxMem].aca.x; est.act.y = mem.Jugadas[mem.ind % maxMem].aca.y;
+                    // Establece la posición del cursor donde acabó el cursor en el último movimiento de bloque
+                    est.act.hor = mem.Jugadas[mem.ind % maxMem].aca.hor; 
+                    est.act.ver = mem.Jugadas[mem.ind % maxMem].aca.ver;
+                    // Movimiento del bloque sin registro
                     est.sel = true;
                     MueveBloque(ref est, dir, ref mem, false);
                     est.sel = false;
                 }
-                mem.Jugadas[mem.ind % maxMem].aca.x = mem.Jugadas[mem.ind % maxMem].emp.x; mem.Jugadas[mem.ind % maxMem].aca.y = mem.Jugadas[mem.ind % maxMem].emp.y;
+                mem.Jugadas[mem.ind % maxMem].aca.hor = mem.Jugadas[mem.ind % maxMem].emp.hor; 
+                mem.Jugadas[mem.ind % maxMem].aca.ver = mem.Jugadas[mem.ind % maxMem].emp.ver;
                 
             }
 
         }
-        static Estado LeeNivel(string file, int n)
+        // Lee un nivel n de un archivo dado
+        static Estado Leeest(string file, int n)
         {
-            Estado nivel; nivel.sel = false; nivel.sal.x = 0; nivel.sal.y = 0; //Inicializo las variables que se van a rellenan más tarde
+            Estado nivel; nivel.sel = false; nivel.sal.hor = 0; nivel.sal.ver = 0; //Inicializo las variables que se van a rellenan más tarde
             StreamReader File = new StreamReader(file);
             string l = "level " + n; //Busco una fila que tenga Level n en el contenido
             while (File.ReadLine() != l) ; //Muevo el cursor del archivo hasta llegar al level correspondiente 
@@ -96,7 +118,7 @@
             {
                 linea = File.ReadLine() + ' ';
                 if (!numColumna) { numColumna = true; numCol = linea.Length; }
-                if (linea == " ") espacio = true;
+                if (linea == " ") { espacio = true; }
                 else { tablero += linea; s++; }
             }
             nivel.mat = new char[numCol + 1, s + 2];
@@ -112,136 +134,155 @@
                 nivel.mat[i, nivel.mat.GetLength(1) - 1] = '#';
             }
             for (int u = 0; u < nivel.mat.GetLength(1); u++) nivel.mat[nivel.mat.GetLength(0) - 1, u] = '#';
-            nivel.act.x = 1; nivel.act.y = 1;
+            nivel.act.hor = 1; nivel.act.ver = 1;
             File.Close();
             return nivel;
         }
-        static void Record(Memoria mem, int level)
-        {
-            StreamReader records = new StreamReader ("record.txt");
-            bool noHabiaJuego = false;
-            while (!(records.ReadLine() == "level " + level || records.EndOfStream))
-                if (records.EndOfStream) noHabiaJuego = true;
-            if (noHabiaJuego)
-                CreaRecord(level, mem);
-            records.Close();
-        }
-        static void CreaRecord(int level, Memoria mem)
-        {
-            StreamWriter record = new StreamWriter("record.txt");
-            record.WriteLine();
-        }
+
+        // Render del estado de juego interno para la consola
         static void Render(Estado est)
         {
+            // Array de colores disponibles para el render
             ConsoleColor[] colores = (ConsoleColor[])
             ConsoleColor.GetValues(typeof(ConsoleColor));
+            // Inicia en la esquina superior izquierda
             Console.SetCursorPosition(0, 0);
             for (int i = 0; i < est.mat.GetLength(0); i++)
             {
                 for (int j = 0; j < est.mat.GetLength(1); j++)
                 {
+                    // Selección de color
                     if (est.mat[i, j] == '#') Console.BackgroundColor = colores[colores.GetLength(0) - 1];
                     else if (est.mat[i, j] == '.') Console.BackgroundColor = colores[0];
                     else Console.BackgroundColor = colores[BloqueToInt(est.mat[i, j])];
-                    Console.Write("  ");
+                    // Escritura del bloque
+                    Console.Write("  "); 
                 }
                 Console.WriteLine();
             }
-            if (est.mat[est.act.y, est.act.x] == '.') Console.BackgroundColor = colores[0];
-            else Console.BackgroundColor = colores[BloqueToInt(est.mat[est.act.y, est.act.x])];
-
-            Console.SetCursorPosition(est.act.x * 2, est.act.y);
-
+            // Selección de color del cursor
+            if (est.mat[est.act.ver, est.act.hor] == '.') Console.BackgroundColor = colores[0];
+            else Console.BackgroundColor = colores[BloqueToInt(est.mat[est.act.ver, est.act.hor])];
+            Console.SetCursorPosition(est.act.hor * 2, est.act.ver);
+            // Diferenciación del cursor si está seleccionado o no
             if (est.sel) Console.WriteLine("<>");
             else Console.WriteLine("**");
-            if (est.mat[est.sal.y, est.sal.x] != est.obj)
+            // Render de la salida, solo si no es parte del bloque que se busca
+            if (est.mat[est.sal.ver, est.sal.hor] != est.obj)
             {
-                Console.SetCursorPosition(est.sal.x * 2, est.sal.y);
-                Console.BackgroundColor = colores[0];
+                Console.SetCursorPosition(est.sal.hor * 2, est.sal.ver);
+                Console.BackgroundColor = colores[0]; // La salida siempre es vacía si no ha terminado el juego
                 Console.Write("  ");
             }
-
             Console.SetCursorPosition(0, est.mat.GetLength(0) + 3);
             Console.ResetColor();
         }
-
+        // Convierte un char en un int para array de colores
         static int BloqueToInt(char c)
         {
             return ((int)c) - ((int)'a') + 1;
         }
+        // Establece la salida (condición de victoria) del nivel
         static void MarcaSalida(ref Estado est)
         {
             int fila, colu;
             int i = 0;
             int j = 0;
-            Coor bloqObj; bloqObj.x = 0; bloqObj.y = 0;
+            Coor bloqObj; bloqObj.hor = 0; bloqObj.ver = 0;
             bool encontrado = false;
             while (i < est.mat.GetLength(1) && !encontrado)
             {
                 j = 0;
                 while (j < est.mat.GetLength(0) && !encontrado)
                 {
-                    if (est.mat[i, j] == est.obj) { encontrado = true; bloqObj.y = i; bloqObj.x = j; }
+                    if (est.mat[i, j] == est.obj) { encontrado = true; bloqObj.ver = i; bloqObj.hor = j; }
                     j++;
                 }
                 i++;
             }
-            if (est.mat[bloqObj.y + 1, bloqObj.x] == est.obj) { colu = bloqObj.x; fila = est.mat.GetLength(0) - 1; }
-            else { colu = est.mat.GetLength(0) - 1; fila = bloqObj.y; }
-            est.sal.x = colu; est.sal.y = fila;
+            if (est.mat[bloqObj.ver + 1, bloqObj.hor] == est.obj) { colu = bloqObj.hor; fila = est.mat.GetLength(0) - 1; }
+            else { colu = est.mat.GetLength(0) - 1; fila = bloqObj.ver; }
+            est.sal.hor = colu; est.sal.ver = fila;
             est.mat[fila, colu] = '.';
         }
+        // ---- Movimiento ----
+        #region Métodos de movimiento
+        // Mueve el cursor del jugador en una dirección dir
         static void MueveCursor(ref Estado est, Coor dir)
         {
-            if (est.act.x + dir.x >= 1 && est.act.x + dir.x < est.mat.GetLength(0) - 1) // Comprueba que no se sale de los bordes de juego
-                est.act.x += dir.x; // Movimiento horizontal
-            if (est.act.y + dir.y >= 1 && est.act.y + dir.y < est.mat.GetLength(1) - 1) // Comprueba que no se sale de los bordes de juego
-                est.act.y += dir.y; // Movimiento vertical
+            if (est.act.hor + dir.hor >= 1 && est.act.hor + dir.hor < est.mat.GetLength(0) - 1) // Comprueba que no se sale de los bordes de juego
+                est.act.hor += dir.hor; // Movimiento horizontal
+            else if (est.act.ver + dir.ver >= 1 && est.act.ver + dir.ver < est.mat.GetLength(1) - 1) // Comprueba que no se sale de los bordes de juego
+                est.act.ver += dir.ver; // Movimiento vertical
         }
+        // Mueve un bloque seleccionado en una dirección dada si es posible
+        // A su vez mueve el cursor
         static void MueveBloque(ref Estado est, Coor dir, ref Memoria mem, bool registro)
         {
             if (est.sel) // Solo mueve si hay bloque seleccionado
             {
-                Coor negdir = dir; negdir.x *= -1; negdir.y *= -1; // Dirección contraria
+                Coor negdir = dir; negdir.hor *= -1; negdir.ver *= -1; // Dirección contraria
                 Coor cabeza = BuscaCabeza(dir, est); // Busca la última parte del bloque en la dirección buscada
                 Coor cola = BuscaCabeza(negdir, est); // Cola es la última posición en la dirección contraria del bloque entero (cabeza contraria)
-                char c = est.mat[cabeza.y, cabeza.x]; // Carácter de la cabeza
-                if ((cola.x != cabeza.x || cola.y != cabeza.y) && est.mat[cabeza.y + dir.y, cabeza.x + dir.x] == '.') // Solo lo mueve si el espacio de alante está libre, y la cola no es la misma que la cabeza
+                char c = est.mat[cabeza.ver, cabeza.hor]; // Carácter de la cabeza
+                if ((cola.hor != cabeza.hor || cola.ver != cabeza.ver) && est.mat[cabeza.ver + dir.ver, cabeza.hor + dir.hor] == '.') // Solo lo mueve si el espacio de alante está libre, y la cola no es la misma que la cabeza
                 {
-                    est.mat[cola.y, cola.x] = '.'; // Reemplaza la cola por un espacio en blanco
-                    est.mat[cabeza.y + dir.y, cabeza.x + dir.x] = c; // Reemplaza el espacio frente a la cabeza en dir por el carácter del bloque
-                    
-                    if (registro) mem.Jugadas[mem.ind % maxMem].emp.x = est.act.x; mem.Jugadas[mem.ind % maxMem].emp.y = est.act.y;
-                    MueveCursor(ref est, dir); // Mueve el cursor junto al bloque
+                    est.mat[cola.ver, cola.hor] = '.'; // Reemplaza la cola por un espacio en blanco
+                    est.mat[cabeza.ver + dir.ver, cabeza.hor + dir.hor] = c; // Reemplaza el espacio frente a la cabeza en dir por el carácter del bloque
+                    // Registra la posición del cursor previa al movimiento
                     if (registro)
                     {
-                        mem.Jugadas[mem.ind % maxMem].aca.x = est.act.x; mem.Jugadas[mem.ind % maxMem].aca.y = est.act.y;
+                        mem.Jugadas[mem.ind % maxMem].emp.hor = est.act.hor; 
+                        mem.Jugadas[mem.ind % maxMem].emp.ver = est.act.ver;
+                    }
+                    MueveCursor(ref est, dir); // Mueve el cursor junto al bloque
+                    // Registra la posición del cursor posterior al movimiento y aumenta el indice de jugadas en memoria
+                    if (registro)
+                    {
+                        mem.Jugadas[mem.ind % maxMem].aca.hor = est.act.hor; mem.Jugadas[mem.ind % maxMem].aca.ver = est.act.ver;
                         mem.ind++;
                     }
                 }
             }
         }
+        // Búsqueda de la casilla más alejada en una dirección de un bloque dado
         static Coor BuscaCabeza(Coor dir, Estado est)
         {
             Coor pos = est.act; // Posición a comprobar, empieza en el cursor 
             Coor cabeza = pos; // Posición inicial de la cabeza -> justo en el cursor
-            char c = est.mat[pos.y, pos.x]; // Caracter del bloque
-            while (est.mat[pos.y + dir.y, pos.x + dir.x] == c)
+            char c = est.mat[pos.ver, pos.hor]; // Caracter del bloque
+            while (est.mat[pos.ver + dir.ver, pos.hor + dir.hor] == c)
             {
-                pos.x += dir.x; pos.y += dir.y; // Se avanza en la dirección
+                pos.hor += dir.hor; pos.ver += dir.ver; // Se avanza en la dirección
                 cabeza = pos; // Si coinciden, se reasigna la cabeza
             }
             return cabeza;
         }
+        // Acción de movimiento en una dirección
+        static void Mueve(int hor, int ver, ref Estado est, ref Memoria mem)
+        {
+            // Inicialización de la dirección
+            Coor dir;
+            dir.hor = hor; dir.ver = ver;
+            // Mueve bloque o cursor dependiendo de si hay un bloque seleccionado
+            if (est.sel) MueveBloque(ref est, dir, ref mem, true);
+            else MueveCursor(ref est, dir);
+        }
+        #endregion
+        // ---- Input ----
+        #region Métodos de input
+        // Procesamiento del input del jugador
         static void ProcesaInput(ref Estado est, char c, ref Memoria mem)
         {
             Coor pos = est.act; // Posición del cursor actual (más fácil acceso)
             switch (c)
             {
+                // Selección de bloques
                 case 's':
-                    if (est.mat[pos.y, pos.x] != '#' && est.mat[pos.y, pos.x] != '.') // Comprueba si está el cursor sobre un bloque
+                    if (est.mat[pos.ver, pos.hor] != '#' && est.mat[pos.ver, pos.hor] != '.') // Comprueba si está el cursor sobre un bloque
                         est.sel = !est.sel; // Invierte sel
                     break;
+                // Movimiento
                 case 'u':
                     Mueve(0, -1, ref est, ref mem);
                     break;
@@ -254,16 +295,14 @@
                 case 'r':
                     Mueve(1, 0, ref est, ref mem);
                     break;
+                // Deshacer jugada
+                case 'z': break;
+                // Salida
+                case 'q': break;
                 default: break;
             }
         }
-        static void Mueve(int x, int y, ref Estado est, ref Memoria mem)
-        {
-            Coor dir;
-            dir.x = x; dir.y = y;
-            if (est.sel) MueveBloque(ref est, dir, ref mem, true);
-            else MueveCursor(ref est, dir);
-        }
+        // Lectura de input del jugador
         static char LeeInput()
         {
             char d = ' ';
@@ -286,6 +325,25 @@
             }
             return d;
         }
+        #endregion
+        // ---- Archivo de records ----
+        #region Métodos función records
+        static void Record(Memoria mem, int level)
+        {
+            StreamReader records = new StreamReader("record.txt");
+            bool noHabiaJuego = false;
+            while (!(records.ReadLine() == "level " + level || records.EndOfStream))
+                if (records.EndOfStream) noHabiaJuego = true;
+            if (noHabiaJuego)
+                CreaRecord(level, mem);
+            records.Close();
+        }
+        static void CreaRecord(int level, Memoria mem)
+        {
+            StreamWriter record = new StreamWriter("record.txt");
+            record.WriteLine();
+        }
+        #endregion
     }
 }
 
